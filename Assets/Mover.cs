6df4b88394614b73da00774b1f298dc75ae8a621
grizz03 <sawilson1110@yahoo.com>
2021-04-22 20:39:40 -0700

    added instructions

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -8,7 +8,7 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        PrintInstructions();
     }
 
     // Update is called once per frame
