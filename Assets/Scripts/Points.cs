2b8c8e62650c0c73d97a965327cb844f89753210
grizz03 <sawilson1110@yahoo.com>
2021-04-22 21:20:04 -0700

    added point system

--- Assets/Scripts/Points.cs
+++ Assets/Scripts/Points.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Points : MonoBehaviour
 {
+    float points = 10;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log($"You have {points} points");
     }
 
     // Update is called once per frame
@@ -15,4 +16,11 @@ public class Points : MonoBehaviour
     {
         
     }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        --points;
+        Debug.Log($"You have {points} points left");
+    }
+
 }
