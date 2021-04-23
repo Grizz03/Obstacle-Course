6f89b393c592134d7643129881cfca9c1efd4749
grizz03 <sawilson1110@yahoo.com>
2021-04-22 20:31:23 -0700

    added time.deltatime to Player

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -4,11 +4,7 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-
-    [SerializeField] float xValue = 0.01f;
-    [SerializeField] float yValue = 0.01f;
-    [SerializeField] float zValue = 0;
-
+    [SerializeField] float moveSpeed = 3;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +14,19 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(xValue, yValue, zValue);
+        //Time.deltaTime very important so game is fps dependent
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        transform.Translate(xValue, 0, zValue);
     }
+
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game!");
+        Debug.Log("Move your player with WASD or arrow keys");
+        Debug.Log("Don't hit the walls or anything else!");
+    }
+
 }
